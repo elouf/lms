029135a3b442878288c24e9c024f5b24af3481f2
Erwannig <erwannig@studit.fr>
2017-11-10 11:55:24 +0100

    Stats sauvegarde OK

---
+++ src/AppBundle/Entity/UserStatLogin.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserStatLogin
+ *
+ * @ORM\Table(name="UserStatLogin")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserStatLoginRepository")
+ */
+class UserStatLogin
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    protected $user;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateAcces", type="datetime")
+     */
+    protected $dateAcces;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     *
+     * @return UserStatLogin
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set dateAcces
+     *
+     * @param \DateTime $dateAcces
+     *
+     * @return UserStatLogin
+     */
+    public function setDateAcces($dateAcces)
+    {
+        $this->dateAcces = $dateAcces;
+
+        return $this;
+    }
+
+    /**
+     * Get dateAcces
+     *
+     * @return \DateTime
+     */
+    public function getDateAcces()
+    {
+        return $this->dateAcces;
+    }
+
+}
---
+++ src/AppBundle/EventListener/AuthenticationEventListener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use AppBundle\Entity\UserStatLogin;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Router;
+use DateTime;
+
+class AuthenticationEventListener implements AuthenticationSuccessHandlerInterface
+{
+    protected $router;
+    protected $security;
+    protected $container;
+    protected $em;
+    protected $tokenStorage;
+
+    public function __construct(Router $router, $container, $entity_manager, TokenStorage $tokenStorage)
+    {
+        $this->router = $router;
+        $this->container = $container;
+        $this->em = $entity_manager;
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        $stat = new UserStatLogin();
+
+        $stat->setDateAcces(new DateTime());
+        $stat->setUser($user);
+
+        $this->em->persist($stat);
+
+        $this->em->flush();
+        $response = new Response();
+        $response = new RedirectResponse('myCourses');
+        return $response;
+    }
+
+}
