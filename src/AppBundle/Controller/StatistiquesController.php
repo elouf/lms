e931317ac1b7d833f2105f6a600bc0218c4517a1
Erwannig Louf <erwannig@studit.fr>
2018-04-19 16:23:20 +0200

    Stats docs

--- src/AppBundle/Controller/StatistiquesController.php
+++ src/AppBundle/Controller/StatistiquesController.php
@@ -33,4 +33,61 @@ class StatistiquesController extends Controller
         ]);
     }
 
+    /**
+     * @Route("/stats/usersDocs/disc/{discId}", name="statsDocsByUsersDisc")
+     */
+    public function statsDocsByUsersDiscAction (Request $request, $discId)
+    {
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+
+        $disc = $this->getDoctrine()->getRepository('AppBundle:Discipline')->find($discId);
+
+        $assocs = $this->getDoctrine()->getRepository('AppBundle:AssocDocCours')->findAll();
+
+        $userRepo = $this->getDoctrine()->getRepository('AppBundle:User');
+        $users = $userRepo->findAll();
+
+        $myAssocs = array();
+
+        if($assocs) {
+            foreach ($assocs as $assoc) {
+                if($assoc->getCours()->getDiscipline()->getId() == $discId){
+                    $user = $assoc->getDocument()->getProprietaire();
+
+                    $keyUser = array_search($user, array_column($myAssocs, 'user'));
+
+                    if($keyUser !== false){
+                        $keyCours = array_search($assoc->getCours(), array_column($myAssocs[$keyUser]['depots'], 'cours'));
+
+                        if($keyCours !== false){
+                            $myAssocs[$keyUser]['depots'][$keyCours]['nombre']++;
+                        }else{
+                            $depot = array(
+                                'cours' => $assoc->getCours(),
+                                'nombre' => 1
+                            );
+                            array_push($myAssocs[$keyUser]['depots'], $depot);
+                        }
+                    }else{
+                        $depot = array(array(
+                            'cours' => $assoc->getCours(),
+                            'nombre' => 1
+                        ));
+                        $oneUser = array(
+                            'user' => $user,
+                            'depots' => $depot
+                        );
+                        array_push($myAssocs, $oneUser);
+                    }
+                }
+            }
+        }
+
+        return $this->render('stats/statsDocsByUserDisc.html.twig', [
+            'discipline' => $disc,
+            'assocs' => $myAssocs,
+            'users' => $users
+        ]);
+    }
+
 }
