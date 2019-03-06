9f0aec4d51e63d6860b8d7c94b3c27d0cca5cd53
Erwannig <erwannig@studit.fr>
2019-03-06 09:01:32 +0100

    Modifications Statuts et actions en lots sur les inscriptions

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -1243,7 +1243,7 @@ class UsersController extends Controller
     {
         /* @var $user User */
         foreach ($users as $key => $user) {
-            if($key >= 1500 && $key < 1600) {
+            if($key >= 1600 && $key < 1700) {
                 $hasChanged = $this->giveStatut2Role2($roleName, $user, $statutDestName, "Inscription_c");
                 if (!$hasChanged) {
                     $hasChanged = $this->giveStatut2Role2($roleName, $user, $statutDestName, "Inscription_d");
