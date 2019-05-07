d86b4e1e26220ededcea6fa8d476572baf0decd3
Erwannig <erwannig@studit.fr>
2019-05-07 14:11:42 +0200

    évolution documentation

--- src/AppBundle/Controller/ChatController.php
+++ src/AppBundle/Controller/ChatController.php
@@ -212,7 +212,7 @@ class ChatController extends Controller
             $assoc = $em->getRepository('AppBundle:AssocUserChatSession')->findOneBy(array('session' => $session, 'chat' => $chat));
             $user = $assoc->getUser();
 
-            if($user->getId() != $userConnected->getId()){
+            if($user->getId() == $userId){
                 $newPost = new ChatPost();
                 $newPost->setAuteur($user);
                 $newPost->setChat($chat);
