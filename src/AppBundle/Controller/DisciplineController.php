27ad2ffbffca6804f8d64e7a1006d5016449ffd9
Erwannig <erwannig@studit.fr>
2019-06-04 16:26:54 +0200

    évolution calendrier, Référent, droits particuliers

--- src/AppBundle/Controller/DisciplineController.php
+++ src/AppBundle/Controller/DisciplineController.php
@@ -2,10 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Cours;
 use AppBundle\Entity\Discipline;
 use AppBundle\Entity\Inscription_sess;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use DateTime;
@@ -16,7 +23,7 @@ class DisciplineController extends Controller
     /**
      * @Route("/discCoursManag", name="discCoursManag")
      */
-    public function discCoursManagAction (Request $request)
+    public function discCoursManagAction(Request $request)
     {
         $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
 
@@ -25,13 +32,13 @@ class DisciplineController extends Controller
 
         $myDisc = array();
         $i = 0;
-        if($disciplines){
-            foreach($disciplines as $discipline){
+        if ($disciplines) {
+            foreach ($disciplines as $discipline) {
                 $cours = $this->getDoctrine()->getRepository('AppBundle:Cours')->findBy(array('discipline' => $discipline));
                 $myDisc[$i]['discipline'] = $discipline;
 
                 $myDisc[$i]['cours'] = array();
-                if($cours) {
+                if ($cours) {
                     foreach ($cours as $cour) {
                         array_push($myDisc[$i]['cours'], $cour);
                     }
@@ -47,23 +54,25 @@ class DisciplineController extends Controller
     /**
      * @Route("/myCourses/{id}", defaults={"id" = 0}, name="myCourses")
      */
-    public function myCoursesAction (Request $request, $id)
+    public function myCoursesAction(Request $request, $id)
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         date_default_timezone_set('Europe/Paris');
 
-        $repositoryCours = $this->getDoctrine()->getRepository('AppBundle:Cours');
+        $em = $this->getDoctrine();
 
-        $repositoryCoh = $this->getDoctrine()->getRepository('AppBundle:Cohorte');
+        $repositoryCours = $em->getRepository('AppBundle:Cours');
 
-        $repositoryDis = $this->getDoctrine()->getRepository('AppBundle:Discipline');
+        $repositoryCoh = $em->getRepository('AppBundle:Cohorte');
 
-        $repositoryInscrSess = $this->getDoctrine()->getRepository('AppBundle:Inscription_sess');
+        $repositoryDis = $em->getRepository('AppBundle:Discipline');
 
-        $repositoryStatsUsersDocs = $this->getDoctrine()->getRepository('AppBundle:StatsUsersDocs');
+        $repositoryInscrSess = $em->getRepository('AppBundle:Inscription_sess');
 
-        $repositoryDocuments = $this->getDoctrine()->getRepository('AppBundle:Document');
+        $repositoryStatsUsersDocs = $em->getRepository('AppBundle:StatsUsersDocs');
+
+        $repositoryDocuments = $em->getRepository('AppBundle:Document');
 
         $disciplines = $repositoryDis->findBy(array(), array('nom' => 'ASC'));
 
@@ -74,15 +83,15 @@ class DisciplineController extends Controller
         $coursesIndiv = array();
 
         // si ce n'est pas l'admin, on fait le tri
-        if (!($this->getUser()->hasRole('ROLE_SUPER_ADMIN'))){
+        if (!($this->getUser()->hasRole('ROLE_SUPER_ADMIN'))) {
             // on créé un tableau qui contient les disciplines auxquelles l' user est inscrit par cohorte
-            $repositoryInscrCoh = $this->getDoctrine()->getRepository('AppBundle:Inscription_coh');
-            $inscrsCoh = $repositoryInscrCoh->findBy(array('user'=> $this->getUser()));
+            $repositoryInscrCoh = $em->getRepository('AppBundle:Inscription_coh');
+            $inscrsCoh = $repositoryInscrCoh->findBy(array('user' => $this->getUser()));
             $discInscrCoh = array();
-            foreach($inscrsCoh as $inscrCoh){
+            foreach ($inscrsCoh as $inscrCoh) {
                 $cohorte = $repositoryCoh->find($inscrCoh->getCohorte());
-                foreach($cohorte->getDisciplines() as $disc){
-                    if(!in_array($disc, $discInscrCoh)){
+                foreach ($cohorte->getDisciplines() as $disc) {
+                    if (!in_array($disc, $discInscrCoh)) {
                         array_push($discInscrCoh, $disc);
                     }
                 }
@@ -91,33 +100,33 @@ class DisciplineController extends Controller
             $disciplinesArray2Consider = $discInscrCoh;
 
             // on ajoute les disciplines auxquelles le user est inscrit directement
-            $repositoryInscrD = $this->getDoctrine()->getRepository('AppBundle:Inscription_d');
-            $inscrsD = $repositoryInscrD->findBy(array('user'=> $this->getUser()));
-            foreach($inscrsD as $inscrD){
-                if(!in_array($inscrD->getDiscipline(), $disciplinesArray2Consider)){
+            $repositoryInscrD = $em->getRepository('AppBundle:Inscription_d');
+            $inscrsD = $repositoryInscrD->findBy(array('user' => $this->getUser()));
+            foreach ($inscrsD as $inscrD) {
+                if (!in_array($inscrD->getDiscipline(), $disciplinesArray2Consider)) {
                     array_push($disciplinesArray2Consider, $inscrD->getDiscipline());
                 }
             }
 
             // enfin, on ajoute les cours auxquels l'utilisateur est inscrit individuellement (du coup une portion de discipline)
-            $repositoryInscrC = $this->getDoctrine()->getRepository('AppBundle:Inscription_c');
-            $inscrsC = $repositoryInscrC->findBy(array('user'=> $this->getUser()));
-            foreach($inscrsC as $inscrC){
-                if(!in_array($inscrC->getCours()->getDiscipline(), $disciplinesArray2Consider)){
+            $repositoryInscrC = $em->getRepository('AppBundle:Inscription_c');
+            $inscrsC = $repositoryInscrC->findBy(array('user' => $this->getUser()));
+            foreach ($inscrsC as $inscrC) {
+                if (!in_array($inscrC->getCours()->getDiscipline(), $disciplinesArray2Consider)) {
                     array_push($coursesIndiv, $inscrC->getCours());
                 }
             }
         }
-        if ($this->getUser()->hasRole('ROLE_SUPER_ADMIN') || $this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur'){
+        if ($this->getUser()->hasRole('ROLE_SUPER_ADMIN') || $this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur') {
             // on ajoute les cohortes liées pour l'admin pour qu'il puisse accéder aux pages d'inscriptions à ces cohortes
-            for($i=0; $i<count($disciplinesArray2Consider); $i++){
+            for ($i = 0; $i < count($disciplinesArray2Consider); $i++) {
                 $cohLiees[$i] = $repositoryDis->getCohortes($disciplinesArray2Consider[$i]->getId());
             }
         }
 
         $courses = array();
         // on construit le tableau des disciplines/cours complètes
-        for($i=0; $i<count($disciplinesArray2Consider); $i++){
+        for ($i = 0; $i < count($disciplinesArray2Consider); $i++) {
             $courses[$i]["courses"] = array();
             $courses[$i]["sessions"] = array();
             $courses[$i]["sessionsAdmin"] = array();
@@ -126,40 +135,39 @@ class DisciplineController extends Controller
             $courses[$i]["sessionsFinSession"] = array();
             $courses[$i]["discipline"] = $disciplinesArray2Consider[$i];
             $courses[$i]["cohortesLiees"] = array();
-            if ($this->getUser()->hasRole('ROLE_SUPER_ADMIN') || $this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur'){
+            if ($this->getUser()->hasRole('ROLE_SUPER_ADMIN') || $this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur') {
                 $courses[$i]["cohortesLiees"] = $cohLiees[$i];
             }
-            $coursesT = $repositoryCours->findBy(array('discipline' =>$disciplinesArray2Consider[$i]), array('position' => 'ASC'));
-            for($j=0; $j<count($coursesT); $j++){
-                if(!$coursesT[$j]->getSession()) {
+            $coursesT = $repositoryCours->findBy(array('discipline' => $disciplinesArray2Consider[$i]), array('position' => 'ASC'));
+            for ($j = 0; $j < count($coursesT); $j++) {
+                if (!$coursesT[$j]->getSession()) {
                     array_push($courses[$i]["courses"], $coursesT[$j]);
-                }else{
+                } else {
                     $session = $coursesT[$j]->getSession();
                     $currentDate = new DateTime();
                     $inscrSess = $repositoryInscrSess->findOneBy(array('user' => $this->getUser(), 'session' => $session));
                     // on est inscrit et les dates sont bonnes (ou on est admin ou enseignant)
                     $isEns = false;
-                    if($inscrSess){
-                        if($inscrSess->getRole() == "Enseignant"){
+                    if ($inscrSess) {
+                        if ($inscrSess->getRole() == "Enseignant") {
                             $isEns = true;
                         }
                     }
 
-                    if($currentDate >= $session->getDateDebut() &&
+                    if ($currentDate >= $session->getDateDebut() &&
                         $currentDate <= $session->getDateFin() &&
                         ($inscrSess || $this->getUser()->hasRole('ROLE_SUPER_ADMIN') || (($this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur') && $this->getUser()->getConfirmedByAdmin()) || $isEns)
-                        )
-                    {
+                    ) {
                         // on peut rentrer dans la session et on est dans les dates
                         array_push($courses[$i]["sessions"], $coursesT[$j]);
-                    }elseif($currentDate >= $session->getDateDebutAlerte() && $currentDate < $session->getDateFinAlerte()){
+                    } elseif ($currentDate >= $session->getDateDebutAlerte() && $currentDate < $session->getDateFinAlerte()) {
                         // on affiche l'alerte et on permet de s'inscrire
                         array_push($courses[$i]["sessionsAlerte"], $coursesT[$j]);
                         array_push($courses[$i]["sessionsAlerteIsInscrit"], $inscrSess != null);
-                    }elseif($currentDate >= $session->getDateFinAlerte() && $currentDate < $session->getDateFin()){
+                    } elseif ($currentDate >= $session->getDateFinAlerte() && $currentDate < $session->getDateFin()) {
                         // on affiche le message de fin de session
                         array_push($courses[$i]["sessionsFinSession"], $coursesT[$j]);
-                    }elseif($this->getUser()->hasRole('ROLE_SUPER_ADMIN') || $isEns  || (($this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur') && $this->getUser()->getConfirmedByAdmin())){
+                    } elseif ($this->getUser()->hasRole('ROLE_SUPER_ADMIN') || $isEns || (($this->getUser()->getStatut() == 'Responsable' || $this->getUser()->getStatut() == 'Formateur') && $this->getUser()->getConfirmedByAdmin())) {
                         // on peut rentrer dans la session hors des dates
                         array_push($courses[$i]["sessionsAdmin"], $coursesT[$j]);
                     }
@@ -167,15 +175,15 @@ class DisciplineController extends Controller
             }
         }
         // on lui ajoute les cours individuels (avec leurs disciplines)
-        for($j=0; $j<count($coursesIndiv); $j++){
+        for ($j = 0; $j < count($coursesIndiv); $j++) {
             $discExists = false;
-            for($k=0; $k<count($courses); $k++){
-                if($courses[$k]["discipline"] == $coursesIndiv[$j]->getDiscipline()){
+            for ($k = 0; $k < count($courses); $k++) {
+                if ($courses[$k]["discipline"] == $coursesIndiv[$j]->getDiscipline()) {
                     array_push($courses[$k]["courses"], $coursesIndiv[$j]);
                     $discExists = true;
                 }
             }
-            if(!$discExists){
+            if (!$discExists) {
                 $idx = count($courses);
                 $courses[$idx]["discipline"] = $coursesIndiv[$j]->getDiscipline();
                 $courses[$idx]["courses"] = array($coursesIndiv[$j]);
@@ -184,26 +192,84 @@ class DisciplineController extends Controller
 
         // on recherche les infos liées aux documents
         // Comme un accès aux documents de la discipline existe, on doit afficher l'info-bulle si certains n'ont pas été visités
-        for($j=0; $j<count($courses); $j++){
+        for ($j = 0; $j < count($courses); $j++) {
             $docs = $repositoryDocuments->findByDisc($courses[$j]["discipline"], $this->getUser());
             $documents = array_merge($docs[0], $docs[1]);
 
             $nbNewDocs = 0;
-            foreach($documents as $doc){
+            foreach ($documents as $doc) {
                 $stat = $repositoryStatsUsersDocs->findBy(array('user' => $this->getUser(), 'document' => $doc));
-                if(!$stat){
+                if (!$stat) {
                     $nbNewDocs++;
                 }
             }
             $courses[$j]["nbNewDocs"] = $nbNewDocs;
         }
-        return $this->render('discipline/myCourses.html.twig', ['courses' => $courses, 'active' => $id]);
+
+        $disciplinesArray2ConsiderStr = [];
+        for ($j = 0; $j < count($disciplinesArray2Consider); $j++) {
+            $disciplinesArray2ConsiderStr[$disciplinesArray2Consider[$j]->getNom()] = $disciplinesArray2Consider[$j]->getId();
+        }
+
+        // formulaire de création de nouveau cours (pour le référent et l'admin
+        $form = $this->createFormBuilder()
+            ->add('nom', TextType::class, array(
+                'label' => 'Nom '
+            ))
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image',
+                'required' => true,
+                'multiple' => false
+            ])
+
+            ->add('discipline', ChoiceType::class, [
+                'placeholder' => 'Veuillez choisir une discipline',
+                'label' => "Discipline",
+                'choices' => $disciplinesArray2ConsiderStr,
+                "disabled" => false
+            ])
+            ->add('description', CKEditorType::class, [
+                "required" => false
+            ])
+            ->add('accueil', CKEditorType::class, [
+                "required" => false
+            ])
+            ->add('submit', SubmitType::class, array(
+                'label' => 'Valider',
+                'attr' => array('class' => 'button')
+            ))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $cours = new Cours();
+            $cours->setNom($form['nom']->getData());
+            $dis = $repositoryDis->findOneBy(array('id' => $form['discipline']->getData()));
+            $cours->setDiscipline($dis);
+            $cours->setAccueil($form['accueil']->getData());
+            $cours->setDescription($form['description']->getData());
+
+            $cours->setImageFile($form['imageFile']->getData());
+            $cours->upload();
+
+            $em->getManager()->persist($cours);
+
+            $em->getManager()->flush();
+            return $this->redirectToRoute('myCourses');
+
+        }
+
+        return $this->render('discipline/myCourses.html.twig', [
+            'courses' => $courses,
+            'active' => $id,
+            'form' => $form->createView()
+        ]);
     }
 
     /**
      * @Route("/discs", name="disciplines")
      */
-    public function disciplinesAction (Request $request)
+    public function disciplinesAction(Request $request)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Discipline');
         $disciplines = $repository->findAll();
@@ -214,7 +280,7 @@ class DisciplineController extends Controller
     /**
      * @Route("/disc/{id}", name="oneDiscipline")
      */
-    public function oneDisciplineAction (Request $request, $id)
+    public function oneDisciplineAction(Request $request, $id)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Discipline');
         $discipline = $repository->find($id);
@@ -227,7 +293,7 @@ class DisciplineController extends Controller
     /**
      * @Route("/changeActivationDocsDisc_ajax", name="changeActivationDocsDisc_ajax")
      */
-    public function changeActivationDocsDiscAjaxAction (Request $request)
+    public function changeActivationDocsDiscAjaxAction(Request $request)
     {
         if ($request->isXMLHttpRequest()) {
             $em = $this->getDoctrine()->getEntityManager();
@@ -241,7 +307,7 @@ class DisciplineController extends Controller
             $em->persist($disc);
             $em->flush();
 
-            return new JsonResponse(array('action' =>'change Visibility of documents', 'id' => $disc->getId(), 'isVisible' => $disc->getDocsActivated()));
+            return new JsonResponse(array('action' => 'change Visibility of documents', 'id' => $disc->getId(), 'isVisible' => $disc->getDocsActivated()));
         }
 
         return new JsonResponse('This is not ajax!', 400);
@@ -250,7 +316,7 @@ class DisciplineController extends Controller
     /**
      * @Route("/inscrSess_ajax", name="inscrSess_ajax")
      */
-    public function inscrSessAjaxAction (Request $request)
+    public function inscrSessAjaxAction(Request $request)
     {
         if ($request->isXMLHttpRequest()) {
             $em = $this->getDoctrine()->getEntityManager();
@@ -269,14 +335,14 @@ class DisciplineController extends Controller
             $inscr->setSession($session);
             $inscr->setUser($this->getUser());
             $inscr->setDateInscription(new DateTime());
-            if($role){
+            if ($role) {
                 $inscr->setRole($role);
             }
 
             $em->persist($inscr);
             $em->flush();
 
-            return new JsonResponse(array('action' =>'Inscription user session'));
+            return new JsonResponse(array('action' => 'Inscription user session'));
         }
 
         return new JsonResponse('This is not ajax!', 400);
