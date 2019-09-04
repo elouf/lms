d2c12f8f47e7f64626e38a692a73fd6e0c4b888b
Erwannig <erwannig@studit.fr>
2019-09-04 16:30:56 +0200

    évolution calendrier, Référent, droits particuliers

---
+++ src/AppBundle/Admin/LienAdmin.php
@@ -0,0 +1,43 @@
+<?php
+namespace AppBundle\Admin;
+
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class LienAdmin extends AbstractAdmin
+{
+    // EDIT and CREATE
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('nom', 'text')
+            ->add('description', CKEditorType::class, array(
+                'config_name' => 'my_simple_config'
+            ))
+            ->add('cours', 'sonata_type_model')
+        ;
+
+    }
+
+    //FILTERS
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nom')
+            ->add('cours')
+        ;
+    }
+
+    // VIEW ALL IN TABLE
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('nom')
+            ->add('cours')
+        ;
+    }
+}
