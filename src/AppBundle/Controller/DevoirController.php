7ea58e9adbfb70588e1e2abeba2b7f5cb2ad7921
Erwannig <erwannig@studit.fr>
2019-09-10 10:45:06 +0200

    Devoir : pas obligé d'avoir un sujet

--- src/AppBundle/Controller/DevoirController.php
+++ src/AppBundle/Controller/DevoirController.php
@@ -453,14 +453,15 @@ class DevoirController extends Controller
                 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
                 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
                 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', ' '=>'_' );
-            /*$strUser = strtr($devoir->getCours()->getDiscipline()->getAccronyme().'_'.$user->getFirstname().'_'.$user->getLastname(), $unwanted_array );
+            $strUser = strtr($devoir->getCours()->getDiscipline()->getAccronyme().'_'.$user->getFirstname().'_'.$user->getLastname(), $unwanted_array );
+            //$strUser = strtr($devoir->getCours()->getDiscipline()->getAccronyme().'_'.$user->getFirstname().'_'.$user->getLastname(), $unwanted_array );
             $filename = 'Copie_'.$strUser.$copie->getId().'.'.$ext;
             rename($url, $urlDest.$filename);
 
             $copieFichier->setUrl($urlTab[0].'/var'.$urlDestTab[1].$filename);
 
             $em->persist($copieFichier);
-            $em->persist($copie);*/
+            $em->persist($copie);
             $em->flush();
 
             //return new JsonResponse(array('action' =>'upload File', 'id' => $itemId, 'ext' => $ext, 'nom' => $nomDevoir, 'urlTab' => $urlTab, 'urlDestTab' => $urlDestTab));
