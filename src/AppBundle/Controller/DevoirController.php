a96d844e7fa6e6ca89005248e923b9674af7174b
Erwannig <erwannig@studit.fr>
2019-09-10 10:14:20 +0200

    Devoir : pas obligé d'avoir un sujet

--- src/AppBundle/Controller/DevoirController.php
+++ src/AppBundle/Controller/DevoirController.php
@@ -440,12 +440,12 @@ class DevoirController extends Controller
             $urlTab = explode('/web', $currentUrl);
             $urlDestTab = explode('var', $urlDest);
 
-            $copieFichier = new CopieFichier();
+            /*$copieFichier = new CopieFichier();
             $copieFichier->setCopie($copie);
             $copieFichier->setDateRendu(new DateTime());
             $copie->setDateCreation(new DateTime());
             $copieFichier->setNom($nomDevoir);
-/*
+
             $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
                 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
                 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
