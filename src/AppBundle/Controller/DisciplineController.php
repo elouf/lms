badb189e02d310a57faf9a44375c549f1257b6c6
Erwannig Louf <erwannig@studit.fr>
2020-03-27 17:10:17 +0100

    freeaccess sur les disciplines

--- src/AppBundle/Controller/DisciplineController.php
+++ src/AppBundle/Controller/DisciplineController.php
@@ -152,6 +152,8 @@ class DisciplineController extends Controller
         if(!$user){
             $ip = $this->container->get('request_stack')->getCurrentRequest()->getClientIp();
             $ip = $_SERVER['REMOTE_ADDR'];
+            $ip = $request->server->get('REMOTE_ADDR');
+
             // Je recherche s'il y a déjà un click aujourd'hui...
             $free = new FreeAccessStats();
             $free->setIp($ip);
